4102dff3ecfaa6cd522f5f2d5d76c9be86d3a40a
ismael alves <cearaismael1997@gmail.com>
2022-01-15 23:15:59 -0300

    Add new job dividends

--- src/job/index.js
+++ src/job/index.js
@@ -2,12 +2,16 @@ import { CronJob } from "cron";
 import env from "../env";
 import logger from "../logger";
 import * as updateInvestment from "./jobs/updateInvestment";
+import * as updateDividends from "./jobs/updateDividends";
 
 logger.info("Registered service JOB batch is ON");
 
 const jobs = [
     new CronJob(updateInvestment.schedule, async ()=>{
         await updateInvestment.command();
+    }, null, true, env.timezone),
+    new CronJob(updateDividends.schedule, async ()=>{
+        await updateDividends.command();
     }, null, true, env.timezone)
 ];
 
---
+++ src/job/jobs/updateDividends.js
@@ -0,0 +1,32 @@
+import { dividendsService } from "../../services";
+import { format } from "date-fns";
+import knex from "../../db";
+import logger from "../../logger";
+import dividendsType from "../../enum/dividendsType";
+
+const name = "update-divideds";
+const group = "day";
+const schedule = "0 10 * * *";
+const deadline = 180;
+
+const command = async () => {
+    const dividends = await dividendsService.findAll({dueDate: format(new Date(), "yyyy-MM-dd")});
+    await knex.transaction(async (trx) => {
+        await Promise.all(dividends.map(async (dy) => {
+            try {
+                await dividendsService.update({id: dy.id}, {type: dividendsType.PAID}, trx);
+            } catch (error) {
+                logger.error(`Faill to update dividend - error: ${error}`);
+            }
+        }));
+    });
+    return `Execute ${name} done`;
+};
+
+export {
+    command,
+    name,
+    group,
+    schedule,
+    deadline,
+};
