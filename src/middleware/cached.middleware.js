f67aeff9e573cadc1934bad9edc27101f4cccf63
ismael alves <cearaismael1997@gmail.com>
2022-07-25 11:09:21 -0300

    fix lint

--- src/middleware/cached.middleware.js
+++ src/middleware/cached.middleware.js
@@ -6,8 +6,8 @@ import { getCache, setCache } from "../utils/cache";
  */
 export default function cachedMiddleware(timeExp = 20) {
     return async (req, res, next) => {
-        const ip = (req.headers['x-forwarded-for'] || req.socket.remoteAddress).replace(':', '').replace('.', '-')
-        const key = (req.originalUrl || req.url).replace('/', ':').substring(1).concat(ip);
+        const ip = (req.headers["x-forwarded-for"] || req.socket.remoteAddress).replace(":", "").replace(".", "-");
+        const key = (req.originalUrl || req.url).replace("/", ":").substring(1).concat(ip);
         const data = await getCache(key);
         if (data) {
             res.json(data);
