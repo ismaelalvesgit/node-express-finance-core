7401d91b66b6709b7672d63d7c9da2fbf983ea8c
ismael alves <cearaismael1997@gmail.com>
2022-07-25 17:54:07 -0300

    improve cache manager

--- src/middleware/cached.middleware.js
+++ src/middleware/cached.middleware.js
@@ -1,20 +1,25 @@
 import { getCache, setCache } from "../utils/cache";
 
 /**
- * @param {number} timeExp
+ * @param {Object} options
+ * @param {number} options.timeExp
+ * @param {string} options.path
  * @returns {import('express').RequestHandler}
  */
-export default function cachedMiddleware(timeExp = 20) {
+export default function cachedMiddleware(options) {
     return async (req, res, next) => {
+        const { timeExp, path } = options
         const ip = (req.headers["x-forwarded-for"] || req.socket.remoteAddress).replace(":", "").replace(".", "-");
-        const key = (req.originalUrl || req.url).replace("/", ":").substring(1).concat(ip);
+        const urlCache = (req.originalUrl || req.url).replace("/", ":").substring(1)
+        const prefix = path ? path.concat(':', urlCache) : urlCache
+        const key = prefix.concat(ip);
         const data = await getCache(key);
         if (data) {
             res.json(data);
         } else {
             res.sendResponse = res.send;
             res.send = (body) => {
-                setCache(key, body, timeExp);
+                setCache(key, body, timeExp || 300);
                 res.sendResponse(body);
             };
             return next();
