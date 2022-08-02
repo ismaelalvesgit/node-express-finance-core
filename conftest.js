911bd7dd452675e3cec22830617be0ccf85b7b9d
ismael alves <cearaismael1997@gmail.com>
2022-08-02 18:48:33 -0300

    update name

--- conftest.js
+++ conftest.js
@@ -1,7 +1,8 @@
 import { exec, echo, exit } from "shelljs";
 
 setImmediate(()=>{
-    if(exec(`docker run --rm -v ${process.cwd()}:/project openpolicyagent/conftest test --policy opa-docker-security.rego Dockerfile`).code !== 0){
+    /* eslint-disable max-len*/
+    if(exec(`docker run --rm -v ${process.cwd()}:/project openpolicyagent/conftest test --policy docker-security.rego Dockerfile`).code !== 0){
         echo("Error: Conftest failed");
         exit(1);
     }
