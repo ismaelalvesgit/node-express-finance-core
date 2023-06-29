d350b722f37c85dfad617072496c633ddc1ca008
ismael alves <cearaismael1997@gmail.com>
2023-06-28 22:09:29 -0300

    add integration apm

---
+++ src/infrastructure/apm/apm.ts
@@ -0,0 +1,41 @@
+import { tokens } from "@di/tokens";
+import { IApmAdapter } from "@infrastructure/types/IApmAdapter";
+import { inject, injectable } from "tsyringe";
+import apm, { Agent } from "elastic-apm-node";
+import { Config } from "@config/config";
+
+@injectable()
+export default class ApmClient implements IApmAdapter {
+    
+    protected elasticAgent?: Agent
+
+    constructor(
+        @inject(tokens.Config)
+        private config: Config
+    ) {
+        const { apm: {
+            secretToken,
+            serverUrl,
+            apiKey,
+            cloudProvider
+        }, serviceName } = this.config.get()
+        this.elasticAgent = serverUrl.length > 0 ?
+        apm.start({
+            serviceName,
+            secretToken,
+            apiKey,
+            serverUrl,
+            cloudProvider
+        }) : undefined
+    }
+
+    captureError(err: string | Error | apm.ParameterizedMessageObject, callback?: apm.CaptureErrorCallback | undefined): void {
+        if (this.elasticAgent?.isStarted()) {
+            this.elasticAgent.captureError(err, callback);
+        }
+    }
+
+    get Agent(): Agent | undefined {
+        return this.elasticAgent
+    }
+}
