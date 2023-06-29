5265fbb37a0d6d7a4bd31a6b5c6db0a1f2bc83d5
ismael alves <cearaismael1997@gmail.com>
2023-06-28 22:09:12 -0300

    add helper type

---
+++ src/helpers/ICommon.ts
@@ -0,0 +1,44 @@
+export interface IEntity {
+    id: number
+    createdAt: Date
+    updatedAt: Date
+}
+
+export interface IPagination <T> {
+    totalCount: number
+    totalPages: number
+    currentPage: number
+    nextPage?: number
+    previousPage?: number
+    items: T[]
+}
+
+export interface IInnerJoinData { 
+    tableName: string
+    key: string
+    reference: string
+    columns: string[]
+}
+
+export interface IQueryData {
+    page?: number
+    pageSize?: number
+    orderBy?: string
+    filterBy?: string[]
+    orderByDescending?: boolean
+    join?: IInnerJoinData[]
+}
+
+export enum EWhereOperator {
+    Equal = "eq",
+    NotEqual = "ne",
+    GreaterThan = "gt",
+    GreaterThanOrEqual = "ge",
+    LessThan = "lt",
+    LessThanOrEqual = "le",
+    Like = "lk"
+}
+export interface ICacheHandlerParams {
+    path: string
+    timeExp?: number
+}
